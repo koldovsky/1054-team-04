c02651bd4f905fa805a0c42da1b5cdc4a7dd94d7
Myroslava Snesar <137907987+MirraSnesar@users.noreply.github.com>
2023-08-28 11:21:32 +0000

    added carousel

--- js/home-gallery.partial.js
+++ js/home-gallery.partial.js
@@ -1,58 +1,55 @@
-const galleryItemsObject = document.querySelector('.gallery-section-items');
-
 const items = [
-    {
-        img: 'beef',
-        alt: 'Beef'
-    },
-    {
-        img: 'cooking-process',
-        alt: 'cooking process'
-    },
-    {
-        img: 'dessert',
-        alt: 'dessert'
-    },
-    {
-        img: 'cooking-time',
-        alt: 'cooking time'
-    },
-    {
-        img: 'friendly-lunch',
-        alt: 'friendly lunch'
-    },
-    {
-        img: 'fruit-salad',
-        alt: 'fruit salad'
-    },
-    {
-        img: 'serving',
-        alt: 'serving' 
-    },
-    {
-        img: 'soup',
-        alt: 'soup'  
-    },
+    '<div><img src="img/gallery/beef.jpg" alt="Beef"></div>',
+    '<div><img src="img/gallery/cooking-process.jpg" alt="cooking process"></div>',
+    '<div><img src="img/gallery/dessert.jpg" alt="dessert"></div>',
+    '<div><img src="img/gallery/cooking-time.jpg" alt="cooking time"></div>',
+    '<div><img src="img/gallery/friendly-lunch.jpg" alt="friendly lunch"></div>',
+    '<div><img src="img/gallery/fruit-salad.jpg" alt="fruit salad"></div>',
+    '<div><img src="img/gallery/serving.jpg" alt="serving"></div>',
+    '<div><img src="img/gallery/soup.jpg" alt="soup"></div>',
 ]
 
 let currentItem = 0;
 
+const itemConteiner = document.querySelector('.gallery-section-items');
 
-function renderGallery() {
-    galleryItemsObject.innerHTML += 
-    `<img src="img/gallery/${items[currentItem].img}.jpg" alt="${items[currentItem].alt}">`;
+function renderItem () {
+    itemConteiner.innerHTML = items[currentItem];
+    if (window.matchMedia('(min-width: 268px)').matches){
+        let secondItem = currentItem + 1 >= items.length ? 0 : currentItem + 1;
+        itemConteiner.innerHTML += items[secondItem];
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            let thirdItem = secondItem + 1 >= items.length ? 0 : secondItem + 1;
+            itemConteiner.innerHTML += items[thirdItem];
+            thirdItem = thirdItem + 1 >= items.length ? 0 : thirdItem + 1;
+            itemConteiner.innerHTML += items[thirdItem];
+            if (window.matchMedia('(min-width: 992px)').matches){
+                let fourthItem = thirdItem + 1 >= items.length ? 0 : thirdItem + 1;
+                itemConteiner.innerHTML += items[fourthItem];
+                fourthItem = fourthItem + 1 >= items.length ? 0 : fourthItem + 1;
+                itemConteiner.innerHTML += items[fourthItem];
+                fourthItem = fourthItem + 1 >= items.length ? 0 : fourthItem + 1;
+                itemConteiner.innerHTML += items[fourthItem]; 
+            }
+        }
+    }
 }
 
 function nextItem() {
-    currentItem = currentItem >= items.length ? 0 : currentItem + 1;
+    currentItem = currentItem + 1 >= items.length ? 0 : currentItem + 1;
+    renderItem();
 }
+
+renderItem();
+
+const nextButton = document.querySelector('.gallery-button-next');
+nextButton.addEventListener('click', nextItem);
 
 function prevItem() {
-    currentItem = currentItem < 0 ? items.length - 1 : currentItem - 1;
+    currentItem = currentItem - 1 < 0 ? items.length - 1 : currentItem - 1;
+    renderItem();
 }
+const prevButton = document.querySelector('.gallery-button-prev');
+prevButton.addEventListener('click', prevItem);
 
-renderGallery();
-const galleryButtonPrev = document.querySelector('.gallery-button-prev');
-const galleryButtonNext = document.querySelector('.gallery-button-next');
-galleryButtonNext.addEventListener('click', nextItem);
-galleryButtonPrev.addEventListener('click', prevItem);
+window.addEventListener('resize', renderItem);
