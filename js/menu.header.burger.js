d7cdeef007a9268405628862239953831afb47d7
Vadym Markov <trezub123456@gmail.com>
2023-09-07 00:07:55 +0300

    Burger menu

--- js/menu.header.burger.js
+++ js/menu.header.burger.js
@@ -1,14 +1,20 @@
 
 const iconBurger = document.querySelector(".menu__header__burger-icon");
+const menuContainer = document.querySelector(".menu__header__nav__social__container");
 const burgerNavigation = document.querySelector(".menu__header__nav");
 const menuItems = document.querySelector(".menu__header__menu-items");
+const socialMedia = document.querySelector(".menu__header__working-hours-social-media");
 burgerNavigation.addEventListener("click", (e) => {
+  menuContainer.classList.toggle("burger-menu");
   burgerNavigation.classList.toggle("active");
   menuItems.classList.toggle("burger");
+  socialMedia.classList.toggle("burger-menu");
 });
 
 window.addEventListener('resize', function() {
   if (window.innerWidth >= 992) {
-    burgerNavigation.classList.remove("active"); // Видаляємо клас .active
-  }
+    burgerNavigation.classList.remove("active");
+    socialMedia.classList.remove("burger-menu") 
+  } else burgerNavigation.classList.add("active");
+  socialMedia.classList.remove("burger-menu"); 
 });
